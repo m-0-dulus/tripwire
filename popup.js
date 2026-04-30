6504b264ed56d464e215aabe4f15d2d008ae11ab
Ansh Sharma <ansh.right.here@gmail.com>
2026-04-30 14:44:38 +0530

    Add click event for dark pattern scan alert

---
+++ popup.js
@@ -0,0 +1,10 @@
+document.getElementById("scan").addEventListener("click", async () => {
+  let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+
+  chrome.scripting.executeScript({
+    target: { tabId: tab.id },
+    function: () => {
+      alert("dark pattern scan active!");
+    }
+  });
+});
